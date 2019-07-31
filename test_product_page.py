96f2f1ad866968496afc0a782b4b874a32fdaa7d
Vadim Rebrin <vadimrebrin@outlook.com>
2019-07-31 22:42:23 +0300

    Add need review marks

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,7 @@ from pages.cart_page import CartPage
 from pages.login_page import LoginPage
 import time
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
@@ -25,12 +26,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -53,6 +56,7 @@ class TestUserAddToCartFromProductPage():
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_cart(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
         page = ProductPage(browser, link)
